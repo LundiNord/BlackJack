7f214ceed37e64a2a7cff15bca572d579a12cfd4
Jan <jan.schlenk@icloud.com>
2021-04-14 22:11:33 +0200

    a

--- src/com/company/Karte.java
+++ src/com/company/Karte.java
@@ -3,9 +3,12 @@ package com.company;
 public class Karte
 {
     private int wert;   //Ass=1, Zahlen = 1-10, Bild=10
+
     private boolean istAss = false;
-    public Karte(int wert) //Karten mit Werten implementieren
+    private boolean istBube= false;
+    public Karte(int wert, boolean istBube) //Karten mit Werten implementieren
     {
+        this.istBube = istBube;
         this.wert = wert;
         if(wert==1) {       //Asse vieleicht extra behandeln
             istAss = true;
