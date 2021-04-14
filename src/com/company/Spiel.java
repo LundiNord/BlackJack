1b9049154ca0ba8f0063b4907e785daaf2ce8a96
Jan <jan.schlenk@icloud.com>
2021-04-14 23:02:48 +0200

    a

--- src/com/company/Spiel.java
+++ src/com/company/Spiel.java
@@ -32,17 +32,55 @@ public class Spiel {
     }
 
     public Karte KartenErzeugen() {
-        int x =  new java.util.Random().nextInt(53);    //hoffe das 53 richtig ist
-        int wert;
-        if(x<=16) {             //hässlich      geht das besser??
-            wert = 10;
+       int[] Karten= new int [11];      //Array mit den Kartenwerten
+       Karten[0]=1;
+       Karten[1]=2;
+       Karten[2]=3;
+       Karten[3]=4;
+       Karten[4]=5;
+       Karten[5]=6;
+       Karten[6]=7;
+       Karten[7]=8;
+       Karten[8]=9;
+       Karten[9]=10;
+       Karten[10]=11;
+       Karten[11]=12;                  //Bube als 12, muss erkannt werden
+       int x =  new java.util.Random().nextInt(52);
+       int a;
+       if(x<=4){
+           a=Karten[1];
+       }
+       if(x<=8&&x>4){
+           a=Karten[2];
         }
-        else if(16<x&&x<=20) {      //Bild +
-
-        }
-
-        Karte karte1 = new Karte(4);        //Karte mit x als Wert erzeugen
-        return karte1;
+       if(x<=12&&x>8){
+           a=Karten[3];
+       }
+       if(x<=16&&x>12){
+           a=Karten[4];
+       }
+       if(x<=20&&x>16){
+           a=Karten[5];
+       }
+       if(x<=24&&x>20){
+           a=Karten[6];
+       }
+       if(x<=28&&x>20) {
+           a = Karten[7];
+       }
+       if(x<=32&&x>28){
+           a=Karten[8];
+       }
+       if(x<=44&&x>32){
+           a=Karten[9];
+       }
+       if(x<=48&&x>44){
+           a=Karten[11];             //muss noch auf fallsAss() verweisen
+       }
+       if(x<=52&&x>48){
+           a=Karten[11];            //muss noch auf Bube() verweisen
+       }
     }
+
 
 }
