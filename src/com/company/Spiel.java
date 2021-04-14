37b3e8eb92c6d5079ffbe6e5956ce243ba1f30fc
LundiNord <lundi.nord@tutanota.de>
2021-04-14 23:09:42 +0200

    10

--- src/com/company/Spiel.java
+++ src/com/company/Spiel.java
@@ -1,13 +1,10 @@
 package com.company;
+import java.util.ArrayList;
 
 public class Spiel {
-    public int anzahlSpieler;
-    private Dealer dealer1;     //Dealer deklarieren
-    private Spieler spieler1;   //Spieler deklarieren
-    private Spieler spieler2;   //max 5 Spieler         besserer Weg dafür??
-    private Spieler spieler3;
-    private Spieler spieler4;
-    private Spieler spieler5;
+    private int anzahlSpieler;
+    private Dealer dealer1;                             //Dealer deklarieren
+    private ArrayList Player = new ArrayList();        //Array List an Spielern
     private Interface interface1;       //Interface für das Spiel deklarieren
 
     public Spiel(int anzahlSpieler) {
@@ -21,14 +18,15 @@ public class Spiel {
 
     public void Spielstart() {          //Initialisieren des Spiel
         for(int i=0;i<anzahlSpieler;i++) {      //Spieler erzeugen
-           spieler1 = new Spieler(interface1.Einsatz(i));
-
+            Player.add(new Spieler(interface1.Einsatz(i)));
+            //Karte erzeugen und dem Spieler geben
         }
 
     }
     public void debug() {
-        interface1.ShowEinsatz(spieler1.getEinsatz(), 1);
-
+        interface1.ShowEinsatz([Player.get(1)].i), 1);
+        int i;
+        i = [Player(1)].i;
     }
 
     public Karte KarteErzeugen() {
