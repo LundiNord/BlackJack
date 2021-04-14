9ed4196da0337deba9f772823029a13302a86523
LundiNord <lundi.nord@tutanota.de>
2021-04-14 20:59:53 +0200

    5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
+//import javafx.application.Application;
 
-public class Main {
-    private Spiel spiel1;
-    public void main() {
-
+public class Main  {
+    private static Spiel spiel1;
+    public static void main(String[] args) {
         System.out.println("Hello");
         spiel1 = new Spiel(1);
     }
+
 }
