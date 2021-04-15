34fcbe76b8d831f6ed216edc57fe39c778285859
LundiNord <lundi.nord@tutanota.de>
2021-04-15 18:46:04 +0200

    5

--- src/com/company/Teilnehmer.java
+++ src/com/company/Teilnehmer.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.ArrayList;
+import java.lang.Math;
 
 public abstract class Teilnehmer {               //Unterklassen: Spieler, Dealer
     private ArrayList HandBlatt = new ArrayList();      //Array Liste mit den Karten auf der Hand
@@ -16,14 +17,34 @@ public abstract class Teilnehmer {               //Unterklassen: Spieler, Dealer
 
 
     public int handWert() {         //Berechnet den gesamtwert der Karten auf der Hand
-        int x = 0;
+        int x = 0;          //Wert
+        double a = 0;          //Anzahl Asse
         int g = HandBlatt.size();
         for(int i=0;i<g;i++) {
             Karte karte1 = (Karte) HandBlatt.get(i);
             x = x + karte1.getWert();
+            if(karte1.istAss()==true) {     //Anzahl der Asse zählen
+                a = a+1;
+            }
         }
-        //ToDo: Was mit den Assen
+        //ToDo: Was mit den Assen?
+        if(a>0) {           //Sich um Asse kümmern
+           x = AssHändler(x,a);
+        }
         return x;
+    }
+    public int AssHändler(int wert, double anzahlAsse) {       //Alle kombinationen durchrechnen und beste Auswählen
+        wert = (int) (wert-anzahlAsse);
+        int x = (int) Math.pow(2,anzahlAsse);
+        for (int i = 0; i < anzahlAsse; i++) {
+
+
+
+        }
+
+
+
+        return wert;
     }
     public boolean BlackJackDetektor() {        //true: BlackJack vorhanden
         boolean vorhanden = false;
