e6206a9ab86e9de409c8d6bed18c83262b1cf913
LundiNord <77664001+LundiNord@users.noreply.github.com>
2021-04-16 10:29:43 +0200

    Management für mehrere Spiele

---
+++ src/com/company/Management.java
@@ -0,0 +1,14 @@
+
+package com.company;
+
+public class Management extends {     //Management um mehrere Runden zu spielen
+    //ToDo: Weg um Spieler zu speichern
+    //ToDo: Konto für Spieler
+
+    public Management ()
+    {
+
+    }
+    //ToDo: mehrere Runden Spielen
+    
+}
