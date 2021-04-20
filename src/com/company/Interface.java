2506bb174cb2d0eaabe04c010e3e216a2b3d1071
LundiNord <lundi.nord@tutanota.de>
2021-04-20 20:39:59 +0200

    5

--- src/com/company/Interface.java
+++ src/com/company/Interface.java
@@ -7,7 +7,6 @@ public class Interface {
     public Interface() {
         System.out.println("Willkommen bei BlackJack");
     }
-
     public double Einsatz(int spieler) {        //Der wievielte Spieler wird erzeugt ! 0=1
         Scanner scanner = new Scanner(System.in);
         System.out.println("Spieler"+spieler+": Wie hoch soll dein Einsatz sein?");
@@ -16,9 +15,7 @@ public class Interface {
             x= Einsatz(spieler);
         }
         return x;
-
     }
-
     public void ShowEinsatz(double einsatz, int spieler) {
         System.out.println("Spieler"+spieler+": Dein Einsatz ist: "+einsatz);
     }
